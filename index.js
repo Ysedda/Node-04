7e6efec45ba341714276379d3962f35e25d90ec8
Ysedda <arthur.baudoin@icloud.com>
2022-09-14 17:29:36 +0200

    quest done

--- index.js
+++
@@ -1,7 +0,0 @@
-const number = 2;
-
-function doubleIt(value) {
-  return value * 2;
-}
-
-doubleIt(number);
--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 // server.js
 require('dotenv').config() // this line is mandatory when you want to read variables from the '.env' file 
-console.log(`The server will run on port ${process.env.SERVER_PORT}`);
+console.log(`I am ${process.env.NAME}, wilder in ${process.env.CITY} and I love ${process.env.LANGUAGE}`);
